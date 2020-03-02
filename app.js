9503fe87d343f7235ed4552143ebd69cb585ba69
Michael, Ze-Ren Chen <tsejen.tw@gmail.com>
2020-03-02 14:01:44 +0800

    node to write file, redirect

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 // http/https, fs, path, os
 const http = require('http')
-
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     // console.log(req.url, req.method, req.headers)
     const url = req.url
+    const method = req.method
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html')
         res.write("<html>")
         res.write("<header><title>The page</title></header>")
         res.write('<body><form action="/ping" method="POST"><input type="text" name="pong"><button type="submit">send</button></form></body>')
         res.write("</html>")
+        return res.end()
+    }
+    if(url === '/ping' && method === 'POST') {
+        const body = []
+        req.on('data', (chunk)=>{
+            console.log(chunk)
+            body.push(chunk)
+        })
+        req.on('end', ()=>{
+            const parseBody = Buffer.concat(body).toString()
+            console.log(parseBody)
+            const msg = parseBody.split('=')[1]
+            fs.writeFileSync('msg.txt', msg)
+        })
+        res.statusCode = 302
+        res.setHeader('location', '/')
         return res.end()
     }
     res.setHeader('Content-Type', 'text/html')
